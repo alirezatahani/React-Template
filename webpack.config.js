292865c9f302be60ec20f04e786b7141cc2fb501
AlirezaTahani <alireza.tahani0@gmail.com>
2022-05-07 16:44:51 +0400

    add hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-const htmlPlugin = new HtmlWebPackPlugin({
-	template: "./src/index.html",
-	filename: "./index.html",
-});
 module.exports = {
-	mode: "development",
-	mode: "development",
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	plugins: [htmlPlugin],
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./public/index.html'),
+    }),
+  ],
 };
