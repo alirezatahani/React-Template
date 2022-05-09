882d6ee096b5d0d7447a6a74fe38d5610170503d
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-09 16:35:13 +0430

    refactor calculate col spacing

--- src/components/grid/styles/Row.styles.tsx
+++ src/components/grid/styles/Row.styles.tsx
@@ -15,73 +15,45 @@ export const FlexRow = styled.div<RowStyleProps>`
   margin-top: ${({ rowSpace }) => rowSpace && `${rowSpace * -1}rem`};
 
   @media ${devices.xs} {
-    margin-right: ${({ columnSpace }) =>
-      columnSpace && calculateGutterMargin(columnSpace.xs)};
-    margin-left: ${({ columnSpace }) =>
+    ${({ columnSpace }) =>
       columnSpace && calculateGutterMargin(columnSpace.xs)};
   }
   @media ${devices.sm} {
-    margin-right: ${({ columnSpace }) =>
-      columnSpace && calculateGutterMargin(columnSpace.sm)};
-    margin-left: ${({ columnSpace }) =>
+    ${({ columnSpace }) =>
       columnSpace && calculateGutterMargin(columnSpace.sm)};
   }
   @media ${devices.md} {
-    margin-right: ${({ columnSpace }) =>
-      columnSpace && calculateGutterMargin(columnSpace.md)};
-    margin-left: ${({ columnSpace }) =>
+    ${({ columnSpace }) =>
       columnSpace && calculateGutterMargin(columnSpace.md)};
   }
   @media ${devices.lg} {
-    margin-right: ${({ columnSpace }) =>
-      columnSpace && calculateGutterMargin(columnSpace.lg)};
-    margin-left: ${({ columnSpace }) =>
+    ${({ columnSpace }) =>
       columnSpace && calculateGutterMargin(columnSpace.lg)};
   }
   @media ${devices.xl} {
-    margin-right: ${({ columnSpace }) =>
-      columnSpace && calculateGutterMargin(columnSpace.xl)};
-    margin-left: ${({ columnSpace }) =>
+    ${({ columnSpace }) =>
       columnSpace && calculateGutterMargin(columnSpace.xl)};
   }
 
   > * {
     max-width: 100%;
     margin-top: ${({ rowSpace }) => rowSpace && `${rowSpace}rem`};
-    padding-right: ${({ spacing }) =>
-      spacing && calculateGutterPadding(spacing)};
-    padding-left: ${({ spacing }) =>
-      spacing && calculateGutterPadding(spacing)};
+    ${({ spacing }) => spacing && calculateGutterPadding(spacing)};
 
     @media ${devices.xs} {
-      padding-right: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.xs)};
-      padding-left: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.xs)};
+      ${({ columnSpace }) => calculateGutterPadding(columnSpace.xs)};
     }
     @media ${devices.sm} {
-      padding-right: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.sm)};
-      padding-left: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.sm)};
+      ${({ columnSpace }) => calculateGutterPadding(columnSpace.sm)};
     }
     @media ${devices.md} {
-      padding-right: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.md)};
-      padding-left: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.md)};
+      ${({ columnSpace }) => calculateGutterPadding(columnSpace.md)};
     }
     @media ${devices.lg} {
-      padding-right: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.lg)};
-      padding-left: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.lg)};
+      ${({ columnSpace }) => calculateGutterPadding(columnSpace.lg)};
     }
     @media ${devices.xl} {
-      padding-right: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.xl)};
-      padding-left: ${({ columnSpace }) =>
-        columnSpace && calculateGutterPadding(columnSpace.xl)};
+      ${({ columnSpace }) => calculateGutterPadding(columnSpace.xl)};
     }
   }
 `;
--- src/components/grid/utils/utils.ts
+++ src/components/grid/utils/utils.ts
@@ -1,10 +1,16 @@
 const calculateWidth = (num: number) => `width: ${(num * 100) / 12}%`;
 
 const calculateGutterPadding = (value: number) => {
-  return `${value * 0.5}rem`;
+  return `
+  padding-left:${value * 0.5}rem;
+  padding-right:${value * 0.5}rem;
+  `;
 };
 const calculateGutterMargin = (value: number) => {
-  return `${value * -0.5}rem`;
+  return `
+  margin-right: ${value * -0.5}rem;
+  margin-left: ${value * -0.5}rem;
+  `;
 };
 
 export { calculateWidth, calculateGutterPadding, calculateGutterMargin };
