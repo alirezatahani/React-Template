94bf5d81686f7e8bcd81b9dcf146e93a470b00d3
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-09 22:27:43 +0430

    modify grid index and refactore main index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-
-import Row from './components/grid/content/row/Row';
-import Col from './components/grid/content/col/Col';
+import { Row, Col } from './components/grid/Index';
 import GlobalStyle from './global/Global';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <Row columnSpace={{ xs: 2, sm: 5, md: 9, lg: 11 }}>
-        <Col xs={6} sm={4} md={12} lg={9}>
+      <Row columnSpace={{ xs: 2, sm: 5, md: 9, lg: 2 }}>
+        <Col xs={6} sm={4} md={12} lg={11}>
           <div
             style={{
               width: '100%',
@@ -23,7 +20,7 @@ const App: React.FC = () => {
             Salam
           </div>
         </Col>
-        <Col xs={6} sm={8} md={12} lg={3}>
+        <Col xs={6} sm={8} md={12} lg={1}>
           <div
             style={{
               width: '100%',
