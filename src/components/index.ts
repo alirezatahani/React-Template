e4c2139e60e94443b554b4e169275d275fc28331
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-05-16 21:38:39 +0430

    Adding Toggle to the components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Button } from './button';
 import { Row, Col } from './grid/Index';
 import { Input } from './input';
+import { Toggle } from './toggle';
 
-export { Button, Row, Col, Input };
+export { Button, Row, Col, Input, Toggle };
