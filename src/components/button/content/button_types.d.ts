da2dbe5b7441394e8eeb98a437301afb5ce4406c
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-17 15:11:47 +0430

    bugfix/add type and onClick types

--- src/components/button/content/button_types.d.ts
+++ src/components/button/content/button_types.d.ts
@@ -1,12 +1,17 @@
 import * as React from 'react';
 
+// const ButtonHTMLTypes = tuple('submit', 'button', 'reset');
+// export type ButtonHTMLType = typeof ButtonHTMLTypes[number];
+
 export type ButtonProps = {
   children: React.ReactNode;
   style?: React.CSSProperties;
   size?: 'sm' | 'md' | 'lg';
   variant?: 'outlined' | 'text' | 'filled';
-  color?: 'primary' | 'secondary' | string;
+  color?: 'primary' | 'secondary' | 'warning' | 'success' | 'danger';
   rounded?: boolean;
   block?: boolean;
   disabled?: boolean;
-};
+  type?: 'submit' | 'reset' | 'button';
+  onClick?: React.MouseEventHandler<HTMLElement>;
+} & Omit<React.ButtonHTMLAttributes<any>, 'type' | 'onClick'>;
