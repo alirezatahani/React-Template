76b5747a8d91969f1a656dd841d58e87b5ad24e3
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-22 11:32:59 +0430

    finish building space component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import { Button } from './button';
 import { Row, Col } from './grid/Index';
 import { Input } from './input';
-
-export { Button, Row, Col, Input };
+import { Space } from './space';
+export { Button, Row, Col, Input, Space };
