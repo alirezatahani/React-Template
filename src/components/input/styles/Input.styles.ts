c8766ad09a878113b619fcbf357a9cd833df573d
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-23 12:00:16 +0430

    fix rebasing

--- src/components/input/styles/Input.styles.ts
+++ src/components/input/styles/Input.styles.ts
@@ -1,68 +1,19 @@
 import styled from 'styled-components';
 import {
+  InputLabelProps,
   InputAddonProps,
   InputContainerProps,
-  InputLabelProps,
-  InputProps,
 } from '../content/input/input_types';
 
-export const StyledInput = styled.input<InputProps>(
-  ({ theme, status, scale, rounded, variant }) => ({
-    ...theme.sizes[scale],
-    ...theme.typography.input,
-    color: theme.palette.common.black,
-    outline: 'none',
-    paddingLeft: 11,
-    width: '100%',
-    transition: theme.general.transition,
-    background: variant === 'filled' && theme.palette.greyScale['200'],
-    border: `${
-      variant === 'outlined' || variant === 'filled'
-        ? `solid 1px ${
-            status ? theme.palette[status].main : theme.palette.greyScale['200']
-          }`
-        : `none`
-    }`,
-    borderBottom:
-      variant === 'standard' &&
-      `solid 2px ${status ? theme.palette[status].main : '#00000029'}`,
-    borderRadius: rounded ? theme.general.borderRadius : 4,
-    ['::placeholder']: {
-      opacity: 0.6,
-    },
-
-    [':disabled']: {
-      ...theme.palette.disabled,
-      border: 'none',
-      cursor: 'not-allowed',
-    },
-    ['&:focus']: {
-      outline: `${
-        variant === 'outlined' || variant === 'filled'
-          ? `solid 2px ${
-              status
-                ? theme.palette[status].main
-                : theme.palette['primary'].main
-            }`
-          : `none`
-      }`,
-
-      borderBottom:
-        variant === 'standard' &&
-        `solid 2px ${
-          status ? theme.palette[status].main : theme.palette['primary'].main
-        }`,
-      outlineOffset: 1,
-    },
-  })
-);
-
-export const InputContainer = styled.div<InputContainerProps>({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  margin: '0 0 20px',
-});
+export const InputContainer = styled.div<InputContainerProps>`
+  margin: 0 0 20px;
+  display: ${({ hasAddon }) => (hasAddon ? 'flex' : 'grid')};
+  width: 100%;
+  label {
+    padding-left: 5px;
+    padding-bottom: 10px !important;
+  }
+`;
 
 export const InputLabel = styled.span<InputLabelProps>({
   paddingRight: '20px',
