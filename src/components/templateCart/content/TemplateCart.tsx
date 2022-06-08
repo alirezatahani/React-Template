14af8ed8dd661859536f95fdecac11df09c505c1
Behzad <saeedibehzad@gmail.com>
2022-06-08 17:56:03 +0430

    creating Feature/TemplateCart

---
+++ src/components/templateCart/content/TemplateCart.tsx
@@ -0,0 +1,27 @@
+import { Typography } from '@components/typography';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Cart, CartContainer, CartImage, CartLinks, CartLinkSpan } from '../styles/templateCart.styles';
+import { TemplateCartProps } from './templateCart_types';
+
+const TemplateCart: React.FC<TemplateCartProps> = ({template,src}) => {
+  return (
+    <CartContainer>
+      <Cart>
+        <CartImage
+          src={src}
+        >
+          <CartLinks>
+            <Link
+              to={template.path}
+            >
+              <CartLinkSpan>Full site preview</CartLinkSpan>
+            </Link>
+          </CartLinks>
+        </CartImage>
+      </Cart>
+      <Typography variant="body1">{template.path.replace('/', ' #')}</Typography>
+    </CartContainer>
+  );
+};
+export default TemplateCart;
