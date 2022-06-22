31a00cf3aced80e83b811c15399359cae331b6b9
Behzad <saeedibehzad@gmail.com>
2022-06-22 11:28:34 +0430

    add description popup to Modal & fix title divider bug

--- src/components/modal/content/Modal.tsx
+++ src/components/modal/content/Modal.tsx
@@ -1,7 +1,8 @@
-import { Divider } from '@components/divider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FaTimes } from 'react-icons/fa';
+import { Divider } from '@components/divider';
+import { ModalProps } from './modal_types';
 import {
   ModalClose,
   ModalContainer,
@@ -10,7 +11,14 @@ import {
   ModalTitle,
   ModalWrapper,
 } from '../styles/modal.styles';
-import { ModalProps } from './modal_types';
+/**
+ * @children should be a React element
+ * @visible should be set true/false when you enable/disable modal
+ * @onClose should be set to close modal
+ * @title  can be set modal title
+ * @actions can be set as ReactElement | ReactElement[]
+ * @closeClickOut can be set as true | false when you want close modal when clicked outside
+ */
 
 const Modal: React.FC<ModalProps> = ({ visible, ...props }: ModalProps) => {
   return ReactDOM.createPortal(
@@ -19,25 +27,24 @@ const Modal: React.FC<ModalProps> = ({ visible, ...props }: ModalProps) => {
         visible={visible}
         onClick={props.closeClickOut && props.onClose}
       />
-        <ModalSection visible={visible}>
-          <ModalContainer>
+      <ModalSection visible={visible}>
+        <ModalContainer>
           <ModalTitle>{props.title}</ModalTitle>
-            <Divider color='#ccc'/>
-            <ModalContent>{props.children}</ModalContent>
-            {props.actions && <Divider color='#ccc'/>}
-            <ModalContent>{props.actions}</ModalContent>
-            <ModalClose onClick={props.onClose}>
-              <FaTimes size={18} />
-            </ModalClose>
-          </ModalContainer>
-        </ModalSection>
-     
+          {props.title && <Divider color="#ccc" />}
+          <ModalContent>{props.children}</ModalContent>
+          {props.actions && <Divider color="#ccc" />}
+          <ModalContent>{props.actions}</ModalContent>
+          <ModalClose onClick={props.onClose}>
+            <FaTimes size={18} />
+          </ModalClose>
+        </ModalContainer>
+      </ModalSection>
     </>,
     document.getElementById('modal')
   );
 };
 Modal.defaultProps = {
   visible: false,
-  closeClickOut:false,
+  closeClickOut: false,
 };
 export default Modal;
--- src/components/modal/content/modal_types.d.ts
+++ src/components/modal/content/modal_types.d.ts
@@ -1,12 +1,12 @@
-import { JSXElementConstructor, MouseEventHandler, ReactElement, ReactNode } from 'react';
+import { MouseEventHandler, ReactElement, ReactNode } from 'react';
 
 export type ModalProps = {
   visible: boolean;
   children: ReactNode;
   onClose: MouseEventHandler;
-  title?:string;
-  actions?:ReactElement | ReactElement[],
-  closeClickOut?:boolean
+  title?: string;
+  actions?: ReactElement | ReactElement[];
+  closeClickOut?: boolean;
 };
 export type ModalWrapperProps = {
   visible: boolean;
